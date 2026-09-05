1a37f499bdffb6cbe7ac4ea821806930a0fced36
Vatnax <krzwojtal@gmail.com>
2026-09-05 19:24:22 +0200

    Experiment with test_move instead of thin ray casts

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -3,34 +3,14 @@ extends CharacterBody2D
 const SPEED: float = 150.0
 @onready var player: Node2D = get_tree().current_scene.get_node("Player")
 
-var sees_player: bool = false
+var test_vector: Vector2 = Vector2.ZERO
 
-func _physics_process(_delta: float) -> void:
-	var space_state = get_world_2d().direct_space_state
+func _physics_process(_delta: float) -> void:		
+	var dir = global_position.direction_to(player.global_position)
 	
-	var query = PhysicsRayQueryParameters2D.create(
-		global_position, 
-		player.global_position, 
-		collision_mask, 
-		[self.get_rid()]
-	)
-	var result = space_state.intersect_ray(query)
-
-	if result.collider == player:
-		sees_player = true
-		
-		var dir = global_position.direction_to(player.global_position)
+	if not test_move(global_transform, dir):
 		velocity = dir * SPEED
 	else:
-		sees_player = false
 		velocity = Vector2.ZERO
 
 	move_and_slide()
-	
-	queue_redraw()
-
-func _draw() -> void:		
-	var target_local_pos = to_local(player.global_position)
-	var color = Color.GREEN if sees_player else Color.RED
-	
-	draw_line(Vector2.ZERO, target_local_pos, color, 5.0)

302e4a2c840c6fba27e4e87eb7541cfa8691063e
Vatnax <krzwojtal@gmail.com>
2026-09-05 21:19:42 +0200

    Choose best angle to move towards player

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -9,6 +9,17 @@ func _physics_process(_delta: float) -> void:
 	if not test_move(global_transform, dir):
 		velocity = dir * SPEED
 	else:
-		velocity = Vector2.ZERO
-
+		var best_dir = Vector2.ZERO
+		var best_score = float("-inf")
+		
+		var test_degrees = range(-90, 90 + 1, 30)
+		for degree in test_degrees:
+			var candidate = dir.rotated(deg_to_rad(degree))
+			var score = candidate.dot(dir) if not test_move(global_transform, candidate) else float("-inf")
+			
+			if score > best_score:
+				best_dir = candidate
+				best_score = score
+				
+		velocity = best_dir * SPEED
 	move_and_slide()
